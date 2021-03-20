18fabf8f552942990879272e970f49918768c2a8
swh0314 <swh11116@gmail.com>
2021-03-20 16:20:09 +0900

    project real start! 1. 연습했던 file 삭제 2. pages.java에 화면관련 코드 입력

---
+++ src/Pages.java
@@ -0,0 +1,44 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Pages {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Pages window = new Pages();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Pages() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 800, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		frame.setLocationRelativeTo(null);
+		
+	}
+
+}
