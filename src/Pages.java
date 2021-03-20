ef140c7a1da4310398f5d4d64537cca28e5a0935
swh0314 <swh11116@gmail.com>
2021-03-20 16:35:16 +0900

    21.03.20 1. main page에서 list와 insert button 배치, panel로 공간 구분

--- src/Pages.java
+++ src/Pages.java
@@ -1,10 +1,14 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLayeredPane;
+import javax.swing.JButton;
 
 public class Pages {
 
 	private JFrame frame;
+	private final JLayeredPane layeredPane = new JLayeredPane();
 
 	/**
 	 * Launch the application.
@@ -34,11 +38,34 @@ public class Pages {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 800, 600);
+		frame.setBounds(100, 100, 902, 700);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		layeredPane.setBounds(0, 0, 888, 663);
+		frame.getContentPane().add(layeredPane);
+		
+		JPanel btnPanel = new JPanel();
+		btnPanel.setBounds(0, 0, 150, 663);
+		layeredPane.add(btnPanel);
+		btnPanel.setLayout(null);
+		
+		JButton btnList = new JButton("List");
+		btnList.setBounds(12, 82, 126, 50);
+		btnPanel.add(btnList);
+		
+		JButton btnInsert = new JButton("Insert");
+		btnInsert.setBounds(12, 180, 126, 50);
+		btnPanel.add(btnInsert);
+		
+		JPanel insertPanel = new JPanel();
+		insertPanel.setBounds(150, 0, 735, 663);
+		layeredPane.add(insertPanel);
+		insertPanel.setLayout(null);
+		
+		JPanel listPanel = new JPanel();
+		listPanel.setBounds(150, 0, 735, 663);
+		layeredPane.add(listPanel);
 		frame.setLocationRelativeTo(null);
 		
 	}
-
 }
