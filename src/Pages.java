5b68617eec14a3773398e511c30836ed9c555976
swh0314 <swh11116@gmail.com>
2021-03-21 18:18:44 +0900

    21.03.21
    -search page 일부 작성
    -insert page radio button group 지정

--- src/Pages.java
+++ src/Pages.java
@@ -1,16 +1,18 @@
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JLayeredPane;
-import javax.swing.JButton;
-import javax.swing.JSeparator;
-import javax.swing.JTable;
-import javax.swing.JLabel;
 import java.awt.Font;
-import javax.swing.JTextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
 import javax.swing.JRadioButton;
-import javax.swing.JList;
+import javax.swing.JTextField;
+import java.awt.FlowLayout;
+import javax.swing.JTable;
 
 public class Pages {
 
@@ -19,6 +21,8 @@ public class Pages {
 	private JTextField txtName;
 	private JTextField txtBirth;
 	private JTextField txtPhone;
+	private JTable infoTable;
+	private JTextField txtSearch;
 
 	/**
 	 * Launch the application.
@@ -51,8 +55,34 @@ public class Pages {
 		frame.setBounds(100, 100, 902, 700);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		
 		layeredPane.setBounds(0, 0, 888, 663);
 		frame.getContentPane().add(layeredPane);
+		
+		JPanel listPanel = new JPanel();
+		listPanel.setBounds(150, 0, 735, 663);
+		layeredPane.add(listPanel);
+		listPanel.setLayout(null);
+		
+		JLabel infoLabel2 = new JLabel("\uC778\uC0AC \uBAA9\uB85D");
+		infoLabel2.setBounds(143, 10, 455, 68);
+		infoLabel2.setHorizontalAlignment(JLabel.CENTER);
+		listPanel.add(infoLabel2);
+		
+		infoTable = new JTable();
+		infoTable.setBounds(31, 97, 678, 541);
+		listPanel.add(infoTable);
+		
+		txtSearch = new JTextField();
+		txtSearch.setBounds(31, 57, 629, 30);
+		listPanel.add(txtSearch);
+		txtSearch.setColumns(10);
+		
+		JLabel btnSearch = new JLabel("search");
+		btnSearch.setBounds(659, 57, 64, 30);
+		listPanel.add(btnSearch);
 		
 		JPanel btnPanel = new JPanel();
 		btnPanel.setBounds(0, 0, 150, 663);
@@ -60,11 +90,11 @@ public class Pages {
 		btnPanel.setLayout(null);
 		
 		JButton btnList = new JButton("List");
-		btnList.setBounds(12, 82, 126, 50);
+		btnList.setBounds(12, 128, 126, 50);
 		btnPanel.add(btnList);
 		
 		JButton btnInsert = new JButton("Insert");
-		btnInsert.setBounds(12, 180, 126, 50);
+		btnInsert.setBounds(12, 210, 126, 50);
 		btnPanel.add(btnInsert);
 		
 		JPanel insertPanel = new JPanel();
@@ -107,6 +137,10 @@ public class Pages {
 		JRadioButton sexWoman = new JRadioButton("\uC5EC");
 		sexWoman.setBounds(166, 185, 39, 23);
 		insertPanel.add(sexWoman);
+		
+		ButtonGroup btnSex = new ButtonGroup();
+		btnSex.add(sexMan);
+		btnSex.add(sexWoman);
 		
 		JLabel phoneLabel = new JLabel("\uC5F0\uB77D\uCC98 : ");
 		phoneLabel.setBounds(324, 181, 84, 30);
@@ -153,10 +187,31 @@ public class Pages {
 		gradeF.setBounds(317, 367, 39, 23);
 		insertPanel.add(gradeF);
 		
-		JPanel listPanel = new JPanel();
-		listPanel.setBounds(150, 0, 735, 663);
-		layeredPane.add(listPanel);
+		ButtonGroup btnGrade = new ButtonGroup();
+		btnGrade.add(gradeS);	btnGrade.add(gradeA);
+		btnGrade.add(gradeB);	btnGrade.add(gradeC);
+		btnGrade.add(gradeD);	btnGrade.add(gradeF);
 		frame.setLocationRelativeTo(null);
+		
+		btnList.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				insertPanel.setVisible(false);
+				listPanel.setVisible(true);
+			}
+			
+		});
+		
+		btnInsert.addActionListener(new ActionListener( ) {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				listPanel.setVisible(false);
+				insertPanel.setVisible(true);
+			}
+			
+		});
 		
 	}
 }
